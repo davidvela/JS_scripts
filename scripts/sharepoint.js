a3c4b33d0068f558c41046a5ec94aea298df96b7
DAVID VELA TIRADO <david.vela.tirado@gmail.com>
2020-10-21 23:32:23 +0200

    test JS scripts

--- scripts/sharepoint.js
+++ scripts/sharepoint.js
@@ -1,11 +1,3 @@
-var i = 0;
-function test(){
-    i++;
-    var str_test ="test:" + i ;
-    console.log(str_test );
-    document.getElementById("p1").innerHTML = str_test;
-}
-
 
 function SetSiteContentTypeTitle()
 {
---
+++ scripts/test.js
@@ -0,0 +1,8 @@
+var i = 0;
+function test(){
+    i++;
+    var str_test ="test:" + i ;
+    console.log(str_test );
+    document.getElementById("p1").innerHTML = str_test;
+}
+
