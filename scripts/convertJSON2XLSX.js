d521b55ec0f02012fbcf00b7eea41a812888c494
David VelaT <david.vela.tirado@gmail.com>
2021-04-03 00:17:39 +0200

    read and export files

--- scripts/convertJSON2XLSX.js
+++ scripts/convertJSON2XLSX.js
@@ -1,10 +1,19 @@
 // https://betterprogramming.pub/convert-json-to-xlsx-file-in-javascript-e7611a975916
+// npm install express --save
+// npm install json2xls --save
+
 //convert.js
-const allUsers = require('../data/sample.json');
+var dataPath = '../data/'
+// const dataPath = '../../../../__data/JSON/'
+
+const exportPath = 'data/'
+//const exportPath = '../../../__data/JSON/'
+
+const allUsers = require(dataPath+'sample.json');
 const fs = require('fs');
 const app = require('express')();
 var json2xls = require('json2xls');
-const filename = 'sample.xlsx';
+const filename = dataPath + 'sample.xlsx';
 app.listen(5050, () => {
   console.log("app is running on port 5050");
   convert();
--- scripts/readJSON.js
+++ scripts/readJSON.js
@@ -1,4 +1,25 @@
-const oJSON = require('../data/sample.json');
+//const dataPath = '../data/'
+const dataPath = '../../../../__data/JSON/'
+const exportPath = '../../../__data/JSON/'
+
+const oJSON = require(dataPath+'sample.json');
+
 var ls_json = JSON.stringify(oJSON)
 // console.log(oJSON)
 console.log(ls_json)
+
+const fs = require('fs');
+var json2xls = require('json2xls');
+const filename = exportPath + 'sample2.xlsx';
+
+var convert = function () {
+  var xls = json2xls(oJSON);
+  fs.writeFileSync(filename, xls, 'binary', (err) => {
+     if (err) {
+           console.log("writeFileSync :", err);
+      }
+    console.log( filename+" file is saved!");
+ });
+}
+
+convert();
